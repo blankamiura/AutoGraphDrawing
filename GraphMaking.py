5002b5f0bb9968c321e420322cf8b095255cfc24
blankamiura <blankamiura@gmail.com>
2017-11-14 14:40:08 +0900

    Change the way to get horizonal datas

--- GraphMaking.py
+++ GraphMaking.py
@@ -8,7 +8,7 @@ class GraphMaking():
     def __init__(self, N):
         self.N  = N
         self.dataSet()
-        #self.graphDraw()
+        self.graphDraw()
 
     def dataSet(self):
         '''
@@ -21,23 +21,19 @@ class GraphMaking():
         self.x      = data[:, 0]
         #鉛直方向に使用するデータを取得
         vertical    = data[0:, 1:(data.shape[1])]
-        self.y      = []
-        self.y.append(map(lambda i: vertical[:, self.N*i:self.N*(i+1)], np.arange((data.shape[1]-1/self.N))))
+        self.y      = [vertical[:, int(self.N*i):int(self.N*(i+1))] for i in np.arange(int((data.shape[1]-1)/self.N))]
 
-        print(self.y[0])
-
-        #平均値及び標準偏差の算出
-        self.mean   = np.empty([0, self.x.shape[0]]) #平均値
-        for i in np.arange(len(self.y)):
-            self.mean   = np.append(self.mean, [self.y[i].mean(axis=1)], axis=0)
+        #平均値と標準偏差の取得
+        self.mean   = [self.y[i].mean(axis=1) for i in np.arange(int((data.shape[1]-1)/self.N))]
+        self.std    = [self.y[i].std(axis=1) for i in np.arange(int((data.shape[1]-1)/self.N))]
 
     def graphDraw(self):
         for i in np.arange(len(self.mean)):
             plt.errorbar(self.x, self.mean[i], yerr=self.std[i], c='Black', markersize=8, fmt=MARKER[i], ecolor='Black', label=LABEL[i])
 
         plt.xlim([0, 144])
-        plt.ylim([0, 36])
-        #plt.show()
+        plt.ylim([0, 50])
+        plt.show()
 
 if __name__ == '__main__':
     GraphMaking(3)
